face594e8db23d04dc92ec377d38c6712342f39c
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2023-09-25 15:05:40 -0500

    Añadiendo archivo con las instrucciones del sql

---
+++ src/sql/script create.sql
@@ -0,0 +1,13 @@
+-- Database: clientes
+
+-- DROP DATABASE IF EXISTS clientes;
+
+CREATE DATABASE clientes
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Colombia.1252'
+    LC_CTYPE = 'Spanish_Colombia.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
